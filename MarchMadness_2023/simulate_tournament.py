40e1b2099adea5066124a610d042d5bcb42bbb31
Joe Zobel <jzobel19@yahoo.com>
2023-03-20 00:15:22 -0400

    end of first weekend

--- MarchMadness_2023/simulate_tournament.py
+++ MarchMadness_2023/simulate_tournament.py
@@ -4,6 +4,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from numpy import random, mean, var
+import math
 
 # import team objects
 from teams import *
@@ -52,21 +53,46 @@ def sim_game(team1, team2, round, points_dict, points_decided, force):
     
     return winner
 
+# This function is based on a logistic regression model
+# developed by Jeremy Frank during the 2023 tournament
 def blowout(winner, loser):
-    seed_diff = loser.seed - winner.seed + 15
+    blowout_pts = 0
 
-    over30 = seed_diff / 125
-    over20 = seed_diff / 100
-    over10 = seed_diff / 75
-    less10 = 1 - over30 - over20 - over10
+    ten_pt = 1 / ( 1 + math.exp(-1 * ( -0.3395 - 0.0785 * winner.seed + 0.09093 * loser.seed ) ))
+    ten_pct_prob = random.random()
 
-    return random.choice([0,1,2,3], p=[less10, over10, over20, over30])
+    # < 10 pt win
+    if(ten_pct_prob > ten_pt):
+        return blowout_pts
+    
+    blowout_pts += 1
+    twenty_pt = 1 / ( 1 + math.exp(-1 * ( -1.5551 - 0.0785 * winner.seed + 0.1107 * loser.seed ) ))
+    twenty_pct_prob = random.random()
+
+    # 10-19 pt win
+    if(twenty_pct_prob > twenty_pt):
+        return blowout_pts
+    
+    blowout_pts += 1
+    thirty_pt = 1 / ( 1 + math.exp(-1 * ( -1.1171 - 0.0551 * winner.seed + 0.0166 * loser.seed ) ))
+    thirty_pct_prob = random.random()
+
+    # 20-29 pt win
+    if(thirty_pct_prob > thirty_pt):
+        return blowout_pts
+    
+    # 30+ pt win
+    blowout_pts += 1
+
+    return blowout_pts
+
 
 #########################################################################################################
-headliner = "Pre-Tournament"
-i = 1
+headliner = "Gonzaga beats TCU"
+i = 53
 
 #########################################################################################################
+
 
 def sim_tournament():
 
@@ -74,58 +100,59 @@ def sim_tournament():
     points_decided = {"Devan": 0, "Jeremy": 0, "Josh": 0, "Justin": 0, "Brant": 0, "Nick": 0, "Joe": 0}
 
     # First Four
-    # 2022
-    mw_firstfour_11    = sim_game(mississippi_state, pittsburgh, -1, points_dict, points_decided, [])
-    west_firstfour_11  = sim_game(arizona_state, nevada, -1, points_dict, points_decided, [])
-    east_firstfour_16  = sim_game(texas_southern, fairleigh_dickinson, -1, points_dict, points_decided, [])
-    south_firstfour_16 = sim_game(texas_am_corpus_christi, southeast_missouri_state, -1, points_dict, points_decided, [])
+    # 2023
+    mw_firstfour_11    = sim_game(mississippi_state, pittsburgh, -1, points_dict, points_decided, [0,1])
+    west_firstfour_11  = sim_game(arizona_state, nevada, -1, points_dict, points_decided, [3, 0])
+    east_firstfour_16  = sim_game(texas_southern, fairleigh_dickinson, -1, points_dict, points_decided, [0,3])
+    south_firstfour_16 = sim_game(texas_am_corpus_christi, southeast_missouri_state, -1, points_dict, points_decided, [1,0])
+
 
     #####  West Region  #####
     # Round of 64
-    # 2022
-    r64_w_game1 = sim_game(kansas, howard, 64, points_dict, points_decided, [])
-    r64_w_game2 = sim_game(arkansas, illinois, 64, points_dict, points_decided, [])
-    r64_w_game3 = sim_game(saint_marys, vanderbilt, 64, points_dict, points_decided, [])
-    r64_w_game4 = sim_game(connecticut, iona, 64, points_dict, points_decided, [])
-    r64_w_game5 = sim_game(tcu, west_firstfour_11, 64, points_dict, points_decided, [])
-    r64_w_game6 = sim_game(gonzaga, grand_canyon, 64, points_dict, points_decided, [])
-    r64_w_game7 = sim_game(northwestern, boise_state, 64, points_dict, points_decided, [])
-    r64_w_game8 = sim_game(ucla, unc_asheville, 64, points_dict, points_decided, [])
+    # 2023
+    r64_w_game1 = sim_game(kansas, howard, 64, points_dict, points_decided, [3,0])
+    r64_w_game2 = sim_game(arkansas, illinois, 64, points_dict, points_decided, [2,0])
+    r64_w_game3 = sim_game(saint_marys, vcu, 64, points_dict, points_decided, [2,0])
+    r64_w_game4 = sim_game(connecticut, iona, 64, points_dict, points_decided, [3,0])
+    r64_w_game5 = sim_game(tcu, west_firstfour_11, 64, points_dict, points_decided, [1,0])
+    r64_w_game6 = sim_game(gonzaga, grand_canyon, 64, points_dict, points_decided, [2,0])
+    r64_w_game7 = sim_game(northwestern, boise_state, 64, points_dict, points_decided, [1,0])
+    r64_w_game8 = sim_game(ucla, unc_asheville, 64, points_dict, points_decided, [4,0])
 
     # Round of 32
-    r32_w_game1 = sim_game(r64_w_game1, r64_w_game2, 32, points_dict, points_decided, []) 
-    r32_w_game2 = sim_game(r64_w_game3, r64_w_game4, 32, points_dict, points_decided, []) 
-    r32_w_game3 = sim_game(r64_w_game5, r64_w_game6, 32, points_dict, points_decided, []) 
-    r32_w_game4 = sim_game(r64_w_game7, r64_w_game8, 32, points_dict, points_decided, []) 
+    r32_w_game1 = sim_game(r64_w_game1, r64_w_game2, 32, points_dict, points_decided, [0,1]) # Kansas vs Arkansas
+    r32_w_game2 = sim_game(r64_w_game3, r64_w_game4, 32, points_dict, points_decided, [0,2]) # Saint Mary's vs UConn
+    r32_w_game3 = sim_game(r64_w_game5, r64_w_game6, 32, points_dict, points_decided, [0,1]) # TCU vs Gonzaga
+    r32_w_game4 = sim_game(r64_w_game7, r64_w_game8, 32, points_dict, points_decided, [0,1]) # Northwestern vs UCLA
 
     # Sweet 16
-    r16_w_game1 = sim_game(r32_w_game1, r32_w_game2, 16, points_dict, points_decided, [])
-    r16_w_game2 = sim_game(r32_w_game3, r32_w_game4, 16, points_dict, points_decided, [])
+    r16_w_game1 = sim_game(r32_w_game1, r32_w_game2, 16, points_dict, points_decided, []) # Arkansas vs UConn
+    r16_w_game2 = sim_game(r32_w_game3, r32_w_game4, 16, points_dict, points_decided, []) # Gonzaga vs UCLA
 
     # Elite 8
     west_winner = sim_game(r16_w_game1, r16_w_game2, 8, points_dict, points_decided, [])
 
     ##### Midwest Region #####
     # Round of 64
-    # 2022
-    r64_mw_game1 = sim_game(houston, northern_kentucky, 64, points_dict, points_decided, [])
-    r64_mw_game2 = sim_game(iowa, auburn, 64, points_dict, points_decided, [])
-    r64_mw_game3 = sim_game(miami, drake, 64, points_dict, points_decided, [])
-    r64_mw_game4 = sim_game(indiana, kent_state, 64, points_dict, points_decided, [])
-    r64_mw_game5 = sim_game(iowa_state, mw_firstfour_11, 64, points_dict, points_decided, [])
-    r64_mw_game6 = sim_game(xavier, kennesaw_state, 64, points_dict, points_decided, [])
-    r64_mw_game7 = sim_game(texas_am, penn_state, 64, points_dict, points_decided, [])
-    r64_mw_game8 = sim_game(texas, colgate, 64, points_dict, points_decided, [])
+    # 2023
+    r64_mw_game1 = sim_game(houston, northern_kentucky, 64, points_dict, points_decided, [2,0])
+    r64_mw_game2 = sim_game(iowa, auburn, 64, points_dict, points_decided, [0,1])
+    r64_mw_game3 = sim_game(miami, drake, 64, points_dict, points_decided, [1,0])
+    r64_mw_game4 = sim_game(indiana, kent_state, 64, points_dict, points_decided, [2,0])
+    r64_mw_game5 = sim_game(iowa_state, mw_firstfour_11, 64, points_dict, points_decided, [0,2])
+    r64_mw_game6 = sim_game(xavier, kennesaw_state, 64, points_dict, points_decided, [1,0])
+    r64_mw_game7 = sim_game(texas_am, penn_state, 64, points_dict, points_decided, [0,2])
+    r64_mw_game8 = sim_game(texas, colgate, 64, points_dict, points_decided, [3,0])
 
     # Round of 32
-    r32_mw_game1 = sim_game(r64_mw_game1, r64_mw_game2, 32, points_dict, points_decided, [])
-    r32_mw_game2 = sim_game(r64_mw_game3, r64_mw_game4, 32, points_dict, points_decided, [])
-    r32_mw_game3 = sim_game(r64_mw_game5, r64_mw_game6, 32, points_dict, points_decided, [])
-    r32_mw_game4 = sim_game(r64_mw_game7, r64_mw_game8, 32, points_dict, points_decided, [])
+    r32_mw_game1 = sim_game(r64_mw_game1, r64_mw_game2, 32, points_dict, points_decided, [2,0]) # Houston vs Auburn
+    r32_mw_game2 = sim_game(r64_mw_game3, r64_mw_game4, 32, points_dict, points_decided, [2,0]) # Miami vs Indiana
+    r32_mw_game3 = sim_game(r64_mw_game5, r64_mw_game6, 32, points_dict, points_decided, [0,2]) # Pittsburgh vs Xavier
+    r32_mw_game4 = sim_game(r64_mw_game7, r64_mw_game8, 32, points_dict, points_decided, [0,1]) # Penn State vs Texas
 
     # Sweet 16
-    r16_mw_game1 = sim_game(r32_mw_game1, r32_mw_game2, 16, points_dict, points_decided, [])
-    r16_mw_game2 = sim_game(r32_mw_game3, r32_mw_game4, 16, points_dict, points_decided, [])
+    r16_mw_game1 = sim_game(r32_mw_game1, r32_mw_game2, 16, points_dict, points_decided, []) # Houston vs Miami
+    r16_mw_game2 = sim_game(r32_mw_game3, r32_mw_game4, 16, points_dict, points_decided, []) # Xavier vs Texas
 
     # Elite 8
     midwest_winner = sim_game(r16_mw_game1, r16_mw_game2, 8, points_dict, points_decided, [])
@@ -133,50 +160,50 @@ def sim_tournament():
 
     ##### East Region    #####
     # Round of 64
-    # 2022
-    r64_e_game1 = sim_game(purdue, east_firstfour_16, 64, points_dict, points_decided, [])
-    r64_e_game2 = sim_game(memphis, florida_atlantic, 64, points_dict, points_decided, [])
-    r64_e_game3 = sim_game(duke, oral_roberts, 64, points_dict, points_decided, [])
-    r64_e_game4 = sim_game(tennessee, louisiana, 64, points_dict, points_decided, [])
-    r64_e_game5 = sim_game(kentucky, providence, 64, points_dict, points_decided, [])
-    r64_e_game6 = sim_game(kansas_state, montana_state, 64, points_dict, points_decided, [])
-    r64_e_game7 = sim_game(michigan_state, usc, 64, points_dict, points_decided, [])
-    r64_e_game8 = sim_game(marquette, vermont, 64, points_dict, points_decided, [])
+    # 2023
+    r64_e_game1 = sim_game(purdue, east_firstfour_16, 64, points_dict, points_decided, [0,1])
+    r64_e_game2 = sim_game(memphis, florida_atlantic, 64, points_dict, points_decided, [0,1])
+    r64_e_game3 = sim_game(duke, oral_roberts, 64, points_dict, points_decided, [3,0])
+    r64_e_game4 = sim_game(tennessee, louisiana, 64, points_dict, points_decided, [1,0])
+    r64_e_game5 = sim_game(kentucky, providence, 64, points_dict, points_decided, [1,0])
+    r64_e_game6 = sim_game(kansas_state, montana_state, 64, points_dict, points_decided, [2,0])
+    r64_e_game7 = sim_game(michigan_state, usc, 64, points_dict, points_decided, [2,0])
+    r64_e_game8 = sim_game(marquette, vermont, 64, points_dict, points_decided, [2,0])
 
     # Round of 32
-    r32_e_game1 = sim_game(r64_e_game1, r64_e_game2, 32, points_dict, points_decided, [])
-    r32_e_game2 = sim_game(r64_e_game3, r64_e_game4, 32, points_dict, points_decided, [])
-    r32_e_game3 = sim_game(r64_e_game5, r64_e_game6, 32, points_dict, points_decided, [])
-    r32_e_game4 = sim_game(r64_e_game7, r64_e_game8, 32, points_dict, points_decided, [])
+    r32_e_game1 = sim_game(r64_e_game1, r64_e_game2, 32, points_dict, points_decided, [0,1]) # Fairleigh Dickinson vs Florida Atlantic
+    r32_e_game2 = sim_game(r64_e_game3, r64_e_game4, 32, points_dict, points_decided, [0,2]) # Duke vs Tennessee
+    r32_e_game3 = sim_game(r64_e_game5, r64_e_game6, 32, points_dict, points_decided, [0,1]) # Kentucky vs Kansas State
+    r32_e_game4 = sim_game(r64_e_game7, r64_e_game8, 32, points_dict, points_decided, [1,0]) # Michigan State vs Marquette
 
     # Sweet 16
-    r16_e_game1 = sim_game(r32_e_game1, r32_e_game2, 16, points_dict, points_decided, [])
-    r16_e_game2 = sim_game(r32_e_game3, r32_e_game4, 16, points_dict, points_decided, [])
+    r16_e_game1 = sim_game(r32_e_game1, r32_e_game2, 16, points_dict, points_decided, []) # Florida Atlantic vs Tennessee
+    r16_e_game2 = sim_game(r32_e_game3, r32_e_game4, 16, points_dict, points_decided, []) # Kansas State vs Michigan State
 
     # Elite 8
     east_winner = sim_game(r16_e_game1, r16_e_game2, 8, points_dict, points_decided, [])
 
     ##### South Region   #####
     # Round of 64
-    # 2022
-    r64_s_game1 = sim_game(alabama, south_firstfour_16, 64, points_dict, points_decided, [])
-    r64_s_game2 = sim_game(maryland, west_virginia, 64, points_dict, points_decided, [])
-    r64_s_game3 = sim_game(san_diego_state, charleston, 64, points_dict, points_decided, [])
-    r64_s_game4 = sim_game(virginia, furman, 64, points_dict, points_decided, [])
-    r64_s_game5 = sim_game(creighton, north_carolina_state, 64, points_dict, points_decided, [])
-    r64_s_game6 = sim_game(baylor, uc_santa_barbara, 64, points_dict, points_decided, [])
-    r64_s_game7 = sim_game(missouri, utah_state, 64, points_dict, points_decided, [])
-    r64_s_game8 = sim_game(arizona, princeton, 64, points_dict, points_decided, [])
+    # 2023
+    r64_s_game1 = sim_game(alabama, south_firstfour_16, 64, points_dict, points_decided, [3,0])
+    r64_s_game2 = sim_game(maryland, west_virginia, 64, points_dict, points_decided, [1,0])
+    r64_s_game3 = sim_game(san_diego_state, charleston, 64, points_dict, points_decided, [1,0])
+    r64_s_game4 = sim_game(virginia, furman, 64, points_dict, points_decided, [0,1])
+    r64_s_game5 = sim_game(creighton, north_carolina_state, 64, points_dict, points_decided, [1,0])
+    r64_s_game6 = sim_game(baylor, uc_santa_barbara, 64, points_dict, points_decided, [2,0])
+    r64_s_game7 = sim_game(missouri, utah_state, 64, points_dict, points_decided, [2,0])
+    r64_s_game8 = sim_game(arizona, princeton, 64, points_dict, points_decided, [0,1])
 
     # Round of 32
-    r32_s_game1 = sim_game(r64_s_game1, r64_s_game2, 32, points_dict, points_decided, [])
-    r32_s_game2 = sim_game(r64_s_game3, r64_s_game4, 32, points_dict, points_decided, [])
-    r32_s_game3 = sim_game(r64_s_game5, r64_s_game6, 32, points_dict, points_decided, [])
-    r32_s_game4 = sim_game(r64_s_game7, r64_s_game8, 32, points_dict, points_decided, [])
+    r32_s_game1 = sim_game(r64_s_game1, r64_s_game2, 32, points_dict, points_decided, [3,0]) # Alabama vs Maryland
+    r32_s_game2 = sim_game(r64_s_game3, r64_s_game4, 32, points_dict, points_decided, [3,0]) # San Diego State vs Furman
+    r32_s_game3 = sim_game(r64_s_game5, r64_s_game6, 32, points_dict, points_decided, [1,0]) # Creighton vs Baylor
+    r32_s_game4 = sim_game(r64_s_game7, r64_s_game8, 32, points_dict, points_decided, [0,2]) # Missouri vs Princeton
 
     # Sweet 16
-    r16_s_game1 = sim_game(r32_s_game1, r32_s_game2, 16, points_dict, points_decided, [])
-    r16_s_game2 = sim_game(r32_s_game3, r32_s_game4, 16, points_dict, points_decided, [])
+    r16_s_game1 = sim_game(r32_s_game1, r32_s_game2, 16, points_dict, points_decided, []) # Alabama vs San Diego State
+    r16_s_game2 = sim_game(r32_s_game3, r32_s_game4, 16, points_dict, points_decided, []) # Creighton vs Princeton
 
     # Elite 8
     south_winner = sim_game(r16_s_game1, r16_s_game2, 8, points_dict, points_decided, [])
@@ -187,6 +214,7 @@ def sim_tournament():
 
     # Championship
     champion = sim_game(east_west_winner, south_midwest_winner, 2, points_dict, points_decided, [])
+    champion.championships += 1
 
     return points_dict, points_decided
 
@@ -232,7 +260,8 @@ def sim_many_tournaments(num_sims):
     print("<--- Average points scored --->")
     print(avg_points)
 
-sim_many_tournaments(10000)
+
+sim_many_tournaments(100_000)
 # plot density function for owners with non-zero variance
 legend_list = []
 THICKNESS = 2.5
@@ -240,7 +269,6 @@ THICKNESS = 2.5
 if(var(Devan)):
     sns.kdeplot(Devan, linewidth=THICKNESS, color="tab:blue")
     legend_list.append("Devan:   {0}%".format(num_wins["Devan"]))
-
 
 if(var(Jeremy)):
     sns.kdeplot(Jeremy, linewidth=THICKNESS, color="tab:orange")
@@ -254,10 +282,10 @@ if(var(Justin)):
     sns.kdeplot(Justin, linewidth=THICKNESS, color="tab:red")
     legend_list.append("Justin:  {0}%".format(num_wins["Justin"]))
 
-
-if(var(Brant)):
-    sns.kdeplot(Brant, linewidth=THICKNESS, color="tab:purple")
-    legend_list.append("Brant:   {0}%".format(num_wins["Brant"]))
+# Brant's odds are < 1%
+# if(var(Brant)):
+#     sns.kdeplot(Brant, linewidth=THICKNESS, color="tab:purple")
+#     legend_list.append("Brant:   {0}%".format(num_wins["Brant"]))
 
 if(var(Nick)):
     sns.kdeplot(Nick, linewidth=THICKNESS, color="tab:brown")
@@ -274,4 +302,4 @@ plt.ylabel("")
 plt.yticks([])
 plt.legend(legend_list)
 plt.savefig(f"./mm_figs/mm_{i}.png", dpi=300)
-plt.show()
+#plt.show()
