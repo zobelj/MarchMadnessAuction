cad39be4fb50e77e7688bc71894a1ea9869f0728
Joe Zobel <jzobel19@yahoo.com>
2023-03-20 00:16:54 -0400

    made each image longer, changed number of images to match available

--- MarchMadness_2023/create_gif.py
+++ MarchMadness_2023/create_gif.py
@@ -5,5 +5,5 @@ from PIL import Image
 fp_in = "./mm_figs/mm_*.png"
 fp_out = "./probability_gif.gif"
 
-img, *imgs = [Image.open(f"./mm_figs/mm_{i}.png") for i in range(65)]
-img.save(fp=fp_out, format='gif', append_images=imgs, save_all=True, duration=400, loop=0)
+img, *imgs = [Image.open(f"./mm_figs/mm_{i}.png") for i in range(1, 54)]
+img.save(fp=fp_out, format='gif', append_images=imgs, save_all=True, duration=800, loop=0)
