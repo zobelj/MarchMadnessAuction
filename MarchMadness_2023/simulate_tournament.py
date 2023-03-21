53cd847e1aa186dcc4378db475af385856365cfe
Joe Zobel <jzobel19@yahoo.com>
2023-03-21 00:56:24 -0400

    added arg parser with -n flag for number of sims, implemented into sim scripts

--- MarchMadness_2023/simulate_tournament.py
+++ MarchMadness_2023/simulate_tournament.py
@@ -7,6 +7,7 @@ from lib.teams import *
 from lib.database import create_tables, run_query
 from lib.simulate import sim_game, sim_many_tournaments
 from lib.graphs import density_plot, violin_plot
+from lib.general import get_options
 
 #########################################################################################################
 headliner = "Gonzaga beats TCU"
@@ -192,8 +193,10 @@ def sim_tournament():
 
 
 if __name__ == '__main__':
+    opts = get_options()
+
     create_tables(True)
-    points_lists = sim_many_tournaments(10_000, sim_tournament)
+    points_lists = sim_many_tournaments(opts.num_sims, sim_tournament)
     density_plot(points_lists, headliner, i)
     violin_plot(i=i)
 
