20ff60cabc23fc5652eb0f4eb8bfdeaf39f06440
Joe Zobel <jzobel19@yahoo.com>
2023-03-24 11:51:34 -0400

    changed flask backend routes to accept GET requests

--- MarchMadness_2023/app.py
+++ MarchMadness_2023/app.py
@@ -4,19 +4,18 @@ from lib.prompter import generate_response
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     return "Hello!"
 
-@app.route("/run_prompt")
-def run_prompt():
-    # generate a response given the question in the request
-    question = request.args.get('question')
-    answer = generate_response(question)[0]
+# make an API endpoint that accepts GET requests
+@app.route('/run_prompt/<question>', methods=["GET"])
+def run_prompt(question):
+    # print the request type
+    print(f"Received {request.method} request for question: {question}")
 
-    return {"answer": answer}
+    return { "answer": generate_response(question)[0] }
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5174)
+    app.run(debug=False, port=5174)
